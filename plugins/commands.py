721e3910647eacff44a58f0316073eed49f81048
Naviya2 <74837844+Naviya2@users.noreply.github.com>
2021-06-07 18:22:52 +0530

    Leo Media Serach Bot

--- plugins/commands.py
+++ plugins/commands.py
@@ -19,7 +19,7 @@ async def start(bot, message):
             InlineKeyboardButton('Go Inline', switch_inline_query=''),
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply(START_MSG.format(username=user.username, first_name=user.first_name, last_name=user.last_name))
+        await message.reply(START_MSG.format(username=user.username, first_name=user.first_name, last_name=user.last_name), reply_markup=reply_markup)
 
 
 @Client.on_message(filters.command('channel') & filters.user(ADMINS))
