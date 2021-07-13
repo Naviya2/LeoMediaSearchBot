9082b4b277e7e98b43b14efcff29ff408acddbc7
Navindu Dilmin <74837844+Naviya2@users.noreply.github.com>
2021-07-14 00:03:23 +0530

    Update info.py

--- info.py
+++ info.py
@@ -4,12 +4,14 @@ from os import environ
 id_pattern = re.compile(r'^.\d+$')
 
 # Bot information
-SESSION = environ.get('SESSION', 'Media_search')
+SESSION = environ.get('SESSION', 'LeoMediaSearchBot')
 USER_SESSION = environ.get('USER_SESSION', 'User_Bot')
 API_ID = int(environ['API_ID'])
 API_HASH = environ['API_HASH']
 BOT_TOKEN = environ['BOT_TOKEN']
 USERBOT_STRING_SESSION = environ.get('USERBOT_STRING_SESSION')
+BOT_OWNER = int(os.environ.get("BOT_OWNER", 1069002447))
+BOT_USERNAME = os.environ.get("BOT_USERNAME")
 
 # Bot settings
 MAX_RESULTS = int(environ.get('MAX_RESULTS', 10))
@@ -23,11 +25,18 @@ auth_users = [int(user) if id_pattern.search(user) else user for user in environ
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 auth_channel = environ.get('AUTH_CHANNEL')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else auth_channel
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", -100))
 
 # MongoDB information
 DATABASE_URI = environ['DATABASE_URI']
 DATABASE_NAME = environ['DATABASE_NAME']
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
+
+#for broadcast and force sub
+BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
+UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)
+
+
 
 # Messages
 START_MSG = """
