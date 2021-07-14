c2cdebcac4df8e4a53f915d94559e28383a0ec9a
Navindu Dilmin <74837844+Naviya2@users.noreply.github.com>
2021-07-14 21:29:20 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -35,8 +35,7 @@ async def start(bot, message):
         return
     """Start command handler"""
     if len(message.command) > 1 and message.command[1] == 'subscribe':
-        user = message.from_user
-        await message.reply(INVITE_MSG.format(username=message.from_user.username))
+        await message.reply(INVITE_MSG.format(message.from_user.mention))
     else:
         buttons = [
             [
@@ -48,8 +47,7 @@ async def start(bot, message):
             ],
         ]
         reply_markup = InlineKeyboardMarkup(buttons)
-        user = message.from_user
-        await message.reply(START_MSG.format(username=user.username, first_name=user.first_name, last_name=user.last_name), reply_markup=reply_markup)
+        await message.reply(START_MSG.format(message.from_user.mention), reply_markup=reply_markup)
 
 
 @Client.on_message(filters.command('channel') & filters.user(ADMINS))
