68a3d89d2d9a1c0224b055f7db6bca23278690de
Navindu Dilmin <74837844+Naviya2@users.noreply.github.com>
2021-07-14 19:04:02 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -165,7 +165,7 @@ async def show_status_count(_, bot: Message):
     disk_usage = psutil.disk_usage('/').percent
     total_users = await db.total_users_count()
     await bot.reply_text(
-        text=f"**Total Disk Space:** {total} \n**Used Space:** {used}({disk_usage}%) \n**Free Space:** {free} \n**CPU Usage:** {cpu_usage}% \n**RAM Usage:** {ram_usage}%\n\n**Total Users in DB:** `{total_users}`\n\n@leoinlinesearchbot 🇱🇰",
+        text=f"**Total Disk Space:** {total} \n**Used Space:** {used}({disk_usage}%) \n**Free Space:** {free} \n**CPU Usage:** {cpu_usage}% \n**RAM Usage:** {ram_usage}%\n\n**Total Users in DB:** `{total_users}`\n\n@{info.BOT_USERNAME}",
         parse_mode="Markdown",
         quote=True
     )
