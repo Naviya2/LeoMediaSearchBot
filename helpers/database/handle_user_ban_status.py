cb9d23ad3221a1378cd938133066588acd198568
Navindu Dilmin <74837844+Naviya2@users.noreply.github.com>
2021-07-16 13:27:04 +0530

    Create handle_user_ban_status.py

---
+++ helpers/database/handle_user_ban_status.py
@@ -0,0 +1,19 @@
+# © Naviya2
+
+import info
+from helpers.database.access_db import db
+from pyrogram import Client
+from pyrogram.types import Message
+
+
+async def handle_user_ban_status(bot: Client, cmd: Message):
+ban_status = await db.get_ban_status(chat_id)
+    if ban_status["is_banned"]:
+        if (
+                datetime.date.today() - datetime.date.fromisoformat(ban_status["banned_on"])
+        ).days > ban_status["ban_duration"]:
+            await db.remove_ban(chat_id)
+        else:
+            await cmd.reply_text("You are Banned to Use This Bot 🥺", quote=True)
+            return
+    await cmd.continue_propagation()
