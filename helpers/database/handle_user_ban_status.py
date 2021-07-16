6137e956de151591afac158cfa20b294bbdfee78
Navindu Dilmin <74837844+Naviya2@users.noreply.github.com>
2021-07-16 14:04:29 +0530

    Update handle_user_ban_status.py

--- helpers/database/handle_user_ban_status.py
+++ helpers/database/handle_user_ban_status.py
@@ -7,13 +7,12 @@ from pyrogram.types import Message
 
 
 async def HandleUserBanStatus(bot: Client, cmd: Message):
-chat_id = cmd.from_user.id
-ban_status = await db.get_ban_status(chat_id)
+ban_status = await db.get_ban_status(cmd.from_user.id)
     if ban_status["is_banned"]:
         if (
                 datetime.date.today() - datetime.date.fromisoformat(ban_status["banned_on"])
         ).days > ban_status["ban_duration"]:
-            await db.remove_ban(chat_id)
+            await db.remove_ban(cmd.from_user.id)
         else:
             await cmd.reply_text("You are Banned to Use This Bot 🥺", quote=True)
             return
