7ef5fb45a3d35adcf7e9443ff0bc6e832e820190
Navindu Dilmin <74837844+Naviya2@users.noreply.github.com>
2021-07-18 22:25:09 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -19,15 +19,10 @@ from helpers.database.database import Database
 from helpers.forcesub import ForceSub
 from helpers.broadcast import broadcast_handler
 from helpers.database.add_user import AddUserToDatabase
-from helpers.database.handle_user_ban_status import handle_user_ban_status
 from helpers.humanbytes import humanbytes
 from utils import Media
 
 logger = logging.getLogger(__name__)
-
-@Client.on_message(filters.private)
-async def _(bot, message):
-    await handle_user_ban_status(bot, message)
 
 
 @Client.on_message(filters.command('start'))
@@ -173,102 +168,3 @@ async def show_status_count(_, bot: Message):
         parse_mode="Markdown",
         quote=True
     )
-
-    
-@Client.on_message(filters.private & filters.command("banuser") & filters.user(info.BOT_OWNER))
-async def ban(bot, message):
-    
-    if len(message.command) == 1:
-        await message.reply_text(
-            f"Use this command to ban any user from the bot 😊\n\nUsage:\n\n`/banuser user_id ban_duration ban_reason`\n\nEg: `/banuser 1234567 28 You misused me.`\n This will ban user with id `1234567` for `28` days for the reason `You misused me` 😊",
-            quote=True
-        )
-        return
-
-    try:
-        user_id = int(message.command[1])
-        ban_duration = int(message.command[2])
-        ban_reason = ' '.join(message.command[3:])
-        ban_log_text = f"Banning user {user_id} for {ban_duration} days for the reason {ban_reason}."
-        try:
-            await bot.send_message(
-                user_id,
-                f"You are banned to use this bot for **{ban_duration}** day(s) for the reason __{ban_reason}__ ☹️\n\n**Message from the Bot Admin**"
-            )
-            ban_log_text += '\n\nUser notified successfully 😊'
-        except:
-            traceback.print_exc()
-            ban_log_text += f"\n\nUser notification failed ☹️\n\n`{traceback.format_exc()}`"
-
-        await db.ban_user(user_id, ban_duration, ban_reason)
-        print(ban_log_text)
-        await message.reply_text(
-            ban_log_text,
-            quote=True
-        )
-    except:
-        traceback.print_exc()
-        await message.reply_text(
-            f"Error occoured ❗️ Traceback given below\n\n`{traceback.format_exc()}`",
-            quote=True
-        )
-
-
-@Client.on_message(filters.private & filters.command("unbanuser") & filters.user(info.BOT_OWNER))
-async def unban(bot, message):
-
-    if len(message.command) == 1:
-        await message.reply_text(
-            f"Use this command to unban any user 😊\n\nUsage:\n\n`/unbanuser user_id`\n\nEg: `/unbanuser 1234567`\n This will unban user with id `1234567`.",
-            quote=True
-        )
-        return
-
-    try:
-        user_id = int(message.command[1])
-        unban_log_text = f"Unbanning user {user_id}"
-        try:
-            await bot.send_message(
-                user_id,
-                f"Your ban was lifted 😊"
-            )
-            unban_log_text += '\n\nUser notified successfully 😊'
-        except:
-            traceback.print_exc()
-            unban_log_text += f"\n\nUser notification failed ☹️\n\n`{traceback.format_exc()}`"
-        await db.remove_ban(user_id)
-        print(unban_log_text)
-        await message.reply_text(
-            unban_log_text,
-            quote=True
-        )
-    except:
-        traceback.print_exc()
-        await message.reply_text(
-            f"Error occoured ❗️ Traceback given below\n\n`{traceback.format_exc()}`",
-            quote=True
-        )
-
-
-@Client.on_message(filters.private & filters.command("bannedusers") & filters.user(info.BOT_OWNER))
-async def _banned_usrs(_, bot: Message):
-    
-    all_banned_users = await db.get_all_banned_users()
-    banned_usr_count = 0
-    text = ''
-
-    async for banned_user in all_banned_users:
-        user_id = banned_user['id']
-        ban_duration = banned_user['ban_status']['ban_duration']
-        banned_on = banned_user['ban_status']['banned_on']
-        ban_reason = banned_user['ban_status']['ban_reason']
-        banned_usr_count += 1
-        text += f"> **user_id**: `{user_id}`, **Ban Duration**: `{ban_duration}`, **Banned on**: `{banned_on}`, **Reason**: `{ban_reason}`\n\n"
-    reply_text = f"Total banned user(s) of Leo Media Search Bot: `{banned_usr_count}`\n\n{text}"
-    if len(reply_text) > 4096:
-        with open('banned-users.txt', 'w') as f:
-            f.write(reply_text)
-        await bot.reply_document('banned-users.txt', True)
-        os.remove('banned-users.txt')
-        return
-    await bot.reply_text(reply_text, True)
