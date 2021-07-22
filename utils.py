34f93dfa6e83662f3e069f2cbe685c2b95c3376d
Navindu Dilmin <74837844+Naviya2@users.noreply.github.com>
2021-07-22 16:23:00 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -9,7 +9,7 @@ from umongo import Instance, Document, fields
 from motor.motor_asyncio import AsyncIOMotorClient
 from marshmallow.exceptions import ValidationError
 
-from info import DATABASE_URI, DATABASE_NAME, COLLECTION_NAME, USE_CAPTION_FILTER, API_ID
+from info import DATABASE_URI, DATABASE_NAME, COLLECTION_NAME, USE_CAPTION_FILTER
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
