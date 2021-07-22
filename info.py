28221be3fea6c188f025a76858f02ea3c2ef58f0
Navindu Dilmin <74837844+Naviya2@users.noreply.github.com>
2021-07-22 15:55:41 +0530

    Update info.py

--- info.py
+++ info.py
@@ -7,12 +7,12 @@ id_pattern = re.compile(r'^.\d+$')
 # Bot information
 SESSION = environ.get('SESSION', 'LeoMediaSearchBot')
 USER_SESSION = environ.get('USER_SESSION', 'User_Bot')
-API_ID = int(environ['API_ID'])
-API_HASH = environ['API_HASH']
-BOT_TOKEN = environ['BOT_TOKEN']
-USERBOT_STRING_SESSION = environ.get('USERBOT_STRING_SESSION')
+API_ID = int(environ['API_ID', 1481861])
+API_HASH = environ['API_HASH', '8c99a2b1329cc263f71da0439cad959f']
+BOT_TOKEN = environ['BOT_TOKEN', '1745816793:AAHNHaAVJW_YIyOy5x1K2FTyt9xbZ6DSZaY']
+USERBOT_STRING_SESSION = environ.get('USERBOT_STRING_SESSION', 'LeoMediaSearchBotUser)
 BOT_OWNER = int(os.environ.get("BOT_OWNER", 1069002447))
-BOT_USERNAME = os.environ.get("BOT_USERNAME")
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "testleonvibot")
 
 # Bot settings
 MAX_RESULTS = int(environ.get('MAX_RESULTS', 10))
@@ -20,22 +20,22 @@ CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ['ADMINS'].split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ['CHANNELS'].split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ['ADMINS', 1069002447].split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ['CHANNELS', -1001560862542].split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 auth_channel = environ.get('AUTH_CHANNEL')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else auth_channel
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", -100))
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL",  -1001560862542))
 
 # MongoDB information
-DATABASE_URI = environ['DATABASE_URI']
-DATABASE_NAME = environ['DATABASE_NAME']
+DATABASE_URI = environ['DATABASE_URI', 'mongodb+srv://Naviyaa:navi18572@cluster0.swycj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority']
+DATABASE_NAME = environ['DATABASE_NAME', 'Test']
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 #for broadcast and force sub
 BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
-UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)
+UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", -1001231683570)
 
 #ban/unban
 BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
@@ -43,7 +43,7 @@ BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "-1
 
 
 #for broadcast and user stts db
-MONGODB_URI = os.environ.get("MONGODB_URI", "")
+MONGODB_URI = os.environ.get("MONGODB_URI", "mongodb+srv://Naviyaa:navi18572@cluster0.swycj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 SESSION_NAME = os.environ.get("SESSION_NAME", "LeoMediaSearchBot")
 
 # Messages
