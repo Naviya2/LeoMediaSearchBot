1f414b9065d170a08c52baca833fc5c579665be5
Navindu Dilmin <74837844+Naviya2@users.noreply.github.com>
2021-07-22 16:24:39 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,7 +17,7 @@ class Bot(Client):
     def __init__(self):
         super().__init__(
             session_name=SESSION,
-            api_id=API_ID,
+            api_id=APP_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
             workers=50,
