1c7908224363ef8edc1600485746248ee1f0a88d
Navindu Dilmin <74837844+Naviya2@users.noreply.github.com>
2021-08-04 00:18:01 +0530

    Update info.py

--- info.py
+++ info.py
@@ -3,59 +3,58 @@ import re
 from os import environ
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+
 id_pattern = re.compile(r'^.\d+$')
 
 # Bot information
-SESSION = environ.get('SESSION', 'Media_search')
+SESSION = environ.get('SESSION', '')
 USER_SESSION = environ.get('USER_SESSION', 'User_Bot')
-APP_ID = int(environ.get('APP_ID', 1481861))
-API_HASH = environ.get('API_HASH', '8c99a2b1329cc263f71da0439cad959f')
-BOT_TOKEN = environ.get('BOT_TOKEN', '1844693567:AAEXgRNBBMcd_QapU6ZBGR_mAD5-OAutzZI')
-USERBOT_STRING_SESSION = environ.get('USERBOT_STRING_SESSION', 'LeoMediaSearchBotUser')
+APP_ID = int(environ['APP_ID'])
+API_HASH = environ['API_HASH']
+BOT_TOKEN = environ['BOT_TOKEN']
+USERBOT_STRING_SESSION = environ.get('USERBOT_STRING_SESSION')
 BOT_OWNER = int(os.environ.get("BOT_OWNER", 1069002447))
-BOT_USERNAME = os.environ.get("BOT_USERNAME", "leoinlinesearchbot")
-AUTH_USERS = int(environ.get('AUTH_USERS')
-
+BOT_USERNAME = os.environ.get("BOT_USERNAME")
 
 # Bot settings
 MAX_RESULTS = int(environ.get('MAX_RESULTS', 10))
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
-USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', "False"))
+USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
 
 # Admins, Channels & Users
-ADMINS = int(environ.get('ADMINS', 1069002447))
-CHANNELS = int(environ.get('CHANNELS', -1001578165465))
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL",  -1001560862542))
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ['ADMINS'].split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ['CHANNELS'].split()]
+auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
+AUTH_USERS = (auth_users + ADMINS) if auth_users else []
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", -100))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', 'mongodb+srv://Erichdaniken:Erichdaniken@cluster0.mqvzd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
-DATABASE_NAME = environ.get('DATABASE_NAME', 'Naviyaofficial')
+DATABASE_URI = environ['DATABASE_URI']
+DATABASE_NAME = environ['DATABASE_NAME']
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 #for broadcast and force sub
-BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", "False"))
-UPDATES_CHANNEL = environ.get("UPDATES_CHANNEL", -1001231683570)
+BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", ""))
+UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)
 
 #ban/unban
 BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
 BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "-1001362659779 -1001255795497").split()))
 
-
 #for broadcast and user stts db
-MONGODB_URI = os.environ.get("MONGODB_URI", "mongodb+srv://Naviyaa:navi18572@cluster0.swycj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+MONGODB_URI = os.environ.get("MONGODB_URI", "")
 SESSION_NAME = os.environ.get("SESSION_NAME", "LeoMediaSearchBot")
-
 # Messages
 default_start_massege = """
 **Hi {}👋
 
-I'm Leo Mod Apps Bot**
+I'm Leo Media Search Bot**
 
-You can start searching mod apps by the "Search Mod Apps 🔎" button below 😊
+You can start searching by the "Search Media 🔎" button below 😊
 """
 
-SHARE_BUTTON_TEXT = """
-Leo Mod Apps Bot 🇱🇰
+default_share_button_text = """
+Leo Mod Apps Search Bot 🇱🇰
 
 Here you can find any mod app by searching its name 😊
 
@@ -66,6 +65,8 @@ Developper : @naviya2 🇱🇰
 """
 
 START_MSG = environ.get('START_MSG', default_start_massege)
+
+SHARE_BUTTON_TEXT = environ.get('SHARE_BUTTON_TEXT', default_share_button_text)
 
 INVITE_MSG = environ.get('INVITE_MSG', 'Please join @.... to use this bot')
 
